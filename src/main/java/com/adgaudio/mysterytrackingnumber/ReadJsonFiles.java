5ab4fa2b968abb72e7a05ebbc03f4e45512c8f56
Alex Gaudio <adgaudio@gmail.com>
2017-07-18 20:18:14 -0400

    ..

--- src/main/java/com/adgaudio/mysterytrackingnumber/ReadJsonFiles.java
+++ src/main/java/com/adgaudio/mysterytrackingnumber/ReadJsonFiles.java
@@ -4,24 +4,47 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Stream;
 
 public final class ReadJsonFiles {
 	public static List<String> getResourceFiles(String path) throws IOException {
 		List<String> filenames = new ArrayList<>();
 
-		InputStream in = ReadJsonFiles.class.getResourceAsStream(path);
-		if (in == null) {
-			in = ReadJsonFiles.class.getClassLoader().getResourceAsStream(path);
+		URI uri;
+		try {
+			uri = ReadJsonFiles.class.getResource(path).toURI();
+		} catch (URISyntaxException e) {
+			uri = null;
 		}
-		try (BufferedReader br = new BufferedReader(new InputStreamReader(in))) {
-			String resource;
-
-			while ((resource = br.readLine()) != null) {
-				filenames.add(path + "/" + resource);
+		Path myPath;
+		if (uri != null && uri.getScheme().equals("jar")) {
+			FileSystem fileSystem = FileSystems.newFileSystem(uri, Collections.<String, Object>emptyMap());
+			myPath = fileSystem.getPath(path);
+		} else {
+			myPath = Paths.get(uri);
+		}
+		System.out.println(myPath);
+		Stream<Path> walk = Files.walk(myPath, 1);
+		for (Iterator<Path> it = walk.iterator(); it.hasNext();) {
+			String tmp = it.next().getFileName().toString();
+			if (tmp.endsWith(".json")) {
+				filenames.add(path + "/" + tmp);
 			}
 		}
+		System.out.println(filenames);
+		walk.close();
+
 		return filenames;
 	}
 
